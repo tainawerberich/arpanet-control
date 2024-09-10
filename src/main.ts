145d3e69ac48fa48826636bedafcd755edf82549
Rafael Borba de Azambuja <44791720+rflazambuja@users.noreply.github.com>
2024-09-10 18:54:05 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.production' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
